dce6d06f2a51ca171610ea9c4f1b594d1a2aa93c
maxxkiss <maxx_kiss@mail.ru>
2022-10-30 16:15:01 +0300

    Добавьте файлы проекта.

---
+++ TestProject2/UnitTest1.cs
@@ -0,0 +1,106 @@
+namespace VehicleUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMove()
+        {
+            // arrange
+            var engine = new DieselEngine(2, 6, 180);
+            var wheels = new[]
+            {
+                new Wheel("Continental"),
+                new Wheel("Continental"),
+                new Wheel("Goodyear"),
+                new Wheel("Goodyear"),
+            };
+            var carrier = new Suspension(wheels);
+            var car = new Car("Ford", "F-150", engine, carrier);
+            // act
+            var result = car.Move();
+            // assert
+            Assert.AreEqual(result, "Vroom");
+        }
+
+        [TestMethod]
+        public void TestBrakeStandingStill()
+        {
+            // arrange
+            var engine = new DieselEngine(2, 6, 180);
+            var wheels = new[]
+            {
+                new Wheel("Continental"),
+                new Wheel("Continental"),
+                new Wheel("Goodyear"),
+                new Wheel("Goodyear"),
+            };
+            var carrier = new Suspension(wheels);
+            var car = new Car("Ford", "F-150", engine, carrier);
+            // act
+            var result = car.Stop();
+            // assert
+            Assert.AreEqual(result, "...");
+        }
+
+        [TestMethod]
+        public void TestBrakeInMotion()
+        {
+            // arrange
+            var engine = new ElectricEngine(350);
+            var props = new[]
+            {
+                new Propeller("HighFly"),
+                new Propeller("HighFly"),
+            };
+            var carrier = new Axis(props);
+            var copter = new Car("Boeing", "CH-47", engine, carrier);
+            // act
+            copter.Move();
+            carrier.Acceleration();
+            var result = copter.Stop();
+            // assert
+            Assert.AreEqual(result, "Foof");
+        }
+
+        [TestMethod]
+        public void TestBrakeInHighSpeedMotion()
+        {
+            // arrange
+            var engine = new DieselEngine(2, 6, 180);
+            var wheels = new[]
+            {
+                new Wheel("Continental"),
+                new Wheel("Continental"),
+                new Wheel("Goodyear"),
+                new Wheel("Goodyear"),
+            };
+            var carrier = new Suspension(wheels);
+            var car = new Car("Ford", "F-150", engine, carrier);
+            // act
+            car.Move();
+            carrier.Acceleration();
+            carrier.Acceleration();
+            carrier.Acceleration();
+            var result = car.Stop();
+            // assert
+            Assert.AreEqual(result, "ShhhShhhShhh");
+        }
+
+        [TestMethod]
+        public void TestBrakeWithoutMovingParts()
+        {
+            // arrange
+            var engine = new DieselEngine(2, 6, 180);
+            var wheels = new Wheel[0];
+            var carrier = new Suspension(wheels);
+            var car = new Car("Ford", "F-150", engine, carrier);
+            // act
+            car.Move();
+            carrier.Acceleration();
+            var result = car.Stop();
+            // assert
+            Assert.AreEqual(result, "...");
+        }
+    }
+}
